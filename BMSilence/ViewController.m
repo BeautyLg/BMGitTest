6dbe51765932a54c7a096944d1c0e16f0cff3ea4
mac <mac@macs-macbook-6.local>
2016-08-29 18:06:07 +0800

    第一次修改

--- BMSilence/ViewController.m
+++ BMSilence/ViewController.m
@@ -16,12 +16,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"1111111");
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    
 }
 
 @end
